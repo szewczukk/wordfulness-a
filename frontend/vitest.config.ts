e04b76bd056ee5f3944cf3e007dcdf6773995ad0
szewczukk <dmitro.szewczuk@gmail.com>
2022-04-30 23:10:33 +0200

    Add code coverage tracking

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'jsdom',
+		coverage: {
+			reporter: ['html'],
+			exclude: [...configDefaults.coverage.exclude, '**/graphql.tsx'],
+		},
 	},
 	resolve: {
 		alias: [{ find: 'src', replacement: '/src' }],
