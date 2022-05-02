8ec476977e7587bfb20ace8ee31b289fbc83507a
szewczukk <dmitro.szewczuk@gmail.com>
2022-05-03 01:02:09 +0200

    Testing Input component

---
+++ frontend/src/components/atoms/Input.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { describe, it } from 'vitest';
+import { screen, render, fireEvent } from '@testing-library/react';
+import Input from './Input';
+
+describe('Testing Input', () => {
+	it('It renders the input', () => {
+		render(<Input placeholder="Hello World" type="text" />);
+
+		expect(screen.getByPlaceholderText('Hello World')).toBeInTheDocument();
+	});
+
+	it('It changes the value on typing', () => {
+		render(<Input placeholder="Hello World" type="text" />);
+
+		const input = screen.getByPlaceholderText('Hello World');
+		fireEvent.change(input, { target: { value: 'aaa' } });
+
+		expect(input).toHaveValue('aaa');
+	});
+});
