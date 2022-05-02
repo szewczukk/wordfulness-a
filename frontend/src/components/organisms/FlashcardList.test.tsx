8f7fbf02193e06991e68cf22ce2cd3857a13148f
szewczukk <dmitro.szewczuk@gmail.com>
2022-05-03 01:22:24 +0200

    Test FlashcardList

---
+++ frontend/src/components/organisms/FlashcardList.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { describe, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import FlashcardList from './FlashcardList';
+
+describe('Testing FlashcardList', () => {
+	it('It renders an empty array without an error', () => {
+		render(<FlashcardList flashcards={[]} onDeleteFlashcard={vi.fn()} />);
+	});
+
+	it('It renders array with flashcards', () => {
+		render(
+			<FlashcardList
+				flashcards={[
+					{ id: '0', front: 'Hello', back: 'World' },
+					{ id: '1', front: 'World', back: 'Hello' },
+				]}
+				onDeleteFlashcard={vi.fn()}
+			/>,
+		);
+
+		expect(screen.getByText(/1 - Hello - World/)).toBeInTheDocument();
+		expect(screen.getByText(/2 - World - Hello/)).toBeInTheDocument();
+	});
+});
